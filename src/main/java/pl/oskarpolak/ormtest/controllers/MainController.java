84bc16faf672d66229ef41337c399cabe561b980
Oskar Polak <biuro@fivarto.pl>
2018-03-09 16:56:14 +0100

    asd

--- src/main/java/pl/oskarpolak/ormtest/controllers/MainController.java
+++ src/main/java/pl/oskarpolak/ormtest/controllers/MainController.java
@@ -4,6 +4,7 @@ package pl.oskarpolak.ormtest.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.oskarpolak.ormtest.models.UserModel;
 import pl.oskarpolak.ormtest.models.repositories.UserRepository;
@@ -22,9 +23,19 @@ public class MainController {
     @GetMapping("/")
     @ResponseBody
     public String index(){
-        List<UserModel> userModels = userRepository.findByAgeGreaterThan(20);
-
+        List<UserModel> userModels = userRepository.findByAgeGreaterThanAndLoginLike(20, "%a%");
         return userModels.toString();
     }
 
+    @GetMapping("/login/{login}/{password}")
+    @ResponseBody
+    public String login(@PathVariable("login") String login,
+                        @PathVariable("password") String password){
+
+        boolean exist = userRepository.existsByLoginAndPassword(login, password);
+        if(exist){
+            return "Witaj zalogowano";
+        }
+        return "Błedne dane!";
+    }
 }
--- src/main/java/pl/oskarpolak/ormtest/models/repositories/UserRepository.java
+++ src/main/java/pl/oskarpolak/ormtest/models/repositories/UserRepository.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Repository;
 import pl.oskarpolak.ormtest.models.UserModel;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserModel, Integer> {
     List<UserModel> findByAgeGreaterThanAndLoginLike(int a, String login);
+    Optional<UserModel> findByLoginAndPassword(String login, String password);
+    boolean existsByLoginAndPassword(String login, String password);
 }
