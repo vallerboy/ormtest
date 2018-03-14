4e243f88a4120f94492cc9dd555ff13e79d87608
Oskar Polak <biuro@fivarto.pl>
2018-03-14 11:33:43 +0100

    bbb

--- src/main/java/pl/oskarpolak/ormtest/controllers/PostController.java
+++ src/main/java/pl/oskarpolak/ormtest/controllers/PostController.java
@@ -7,17 +7,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import pl.oskarpolak.ormtest.models.PostModel;
 import pl.oskarpolak.ormtest.models.forms.PostForm;
 import pl.oskarpolak.ormtest.models.repositories.PostRepository;
+import pl.oskarpolak.ormtest.models.services.UserService;
 
 @Controller
 public class PostController {
 
     final PostRepository postRepository;
+    final UserService userService;
 
     @Autowired
-    public PostController(PostRepository postRepository) {
+    public PostController(PostRepository postRepository, UserService userService) {
         this.postRepository = postRepository;
+        this.userService = userService;
     }
 
     @GetMapping("/addpost")
@@ -29,7 +33,11 @@ public class PostController {
     @PostMapping("/addpost")
     public String addPost(@ModelAttribute("postForm") PostForm postForm){
          //todo validate form
-
+        PostModel postModel = new PostModel(postForm);
+        postModel.setUserId(userService.getUserId());
+        
+        postRepository.save(postModel);
+        return "redirect:/";
     }
     //test
 
