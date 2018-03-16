535958015f66be22d8b3c1a40f4f07252cd50f9e
Oskar Polak <biuro@fivarto.pl>
2018-03-16 09:34:42 +0100

    jii

--- src/main/java/pl/oskarpolak/ormtest/controllers/UserController.java
+++ src/main/java/pl/oskarpolak/ormtest/controllers/UserController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import pl.oskarpolak.ormtest.models.services.UserService;
 
 @Controller
@@ -29,6 +32,10 @@ public class UserController {
         return "profile";
     }
 
-
+    @PostMapping("/user/avatar")
+    public String uploadAvatar(@RequestParam("avatar") MultipartFile file){
+        System.out.println(file.getOriginalFilename() + " : " + file.getContentType());
+        return "redirect:/profile";
+    }
 }
 
