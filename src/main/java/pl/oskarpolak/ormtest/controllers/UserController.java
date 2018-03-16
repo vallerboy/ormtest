db1e397177ec4203c2f4648a2358444821803ee8
Oskar Polak <biuro@fivarto.pl>
2018-03-16 10:04:05 +0100

    jii

--- src/main/java/pl/oskarpolak/ormtest/controllers/UserController.java
+++ src/main/java/pl/oskarpolak/ormtest/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.oskarpolak.ormtest.models.services.UserService;
 
 @Controller
@@ -33,9 +34,18 @@ public class UserController {
     }
 
     @PostMapping("/user/avatar")
-    public String uploadAvatar(@RequestParam("avatar") MultipartFile file){
-        System.out.println(file.getOriginalFilename() + " : " + file.getContentType());
+    public String uploadAvatar(@RequestParam("avatar") MultipartFile file,
+                               RedirectAttributes model){
+        if(!isValidContentType(file.getContentType())){
+            model.addFlashAttribute("info", "Nie przyjmujemy takich plików");
+            return "redirect:/profile";
+        }
+        
         return "redirect:/profile";
+    }
+
+    private boolean isValidContentType(String content){
+        return content.contains("JPEG") || content.contains("PNG") || content.contains("JPG");
     }
 }
 
